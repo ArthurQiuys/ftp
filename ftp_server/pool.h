bd2be6c5d2ae6d1314c6922f144e1f5fb7fbe491
qiuyuanshun <qiuyuanshun@yeah.net>
2016-02-23 16:35:22 +0800

    new servers

---
+++ ftp_server/pool.h
@@ -0,0 +1,31 @@
+#ifndef POOL_H
+#define POOL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define BUSY 1
+#define FREE 0
+
+typedef struct sockaddr_in SA;
+
+typedef struct childInfo
+{
+	pid_t pid;
+	int fd_read;
+	int fd_write;
+	int state;
+	int call_times;
+}childInfo, *pchildInfo;
+
+int childProcessInit(pchildInfo childs, int child_num);
+void child_main(int w, int r);
+#endif
